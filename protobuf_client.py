b25239dfde932e0dc890e3ae9eb972367f4e4c0b
sushantahujaa <sushantahuja35@gmail.com>
2020-11-03 02:38:31 -0500

    Adds the originality form

--- protobuf_client.py
+++ protobuf_client.py
@@ -4,9 +4,9 @@ import batch_pb2, requests
 batch_request = batch_pb2.Request()
 
 id = input('Please Enter Request For Workload (RFW) ID:')
-bench_type = input('Please Enter one of the following:\n 1. DVD-testing\n 2. DVD-training\n 3. NDBench-training\n'
+bench_type = input('Please type one of the following:\n 1. DVD-testing\n 2. DVD-training\n 3. NDBench-training\n'
                    '4. NDBench-training\n')
-metric = input('Please choose one of the metrics from the following:\n'
+metric = input('Please type one of the metrics from the following:\n'
                '1. CPUUtilization_Average\n 2. NetworkIn_Average\n 3. NetworkOut_Average\n'
                ' 4. MemoryUtilization_Average\n')
 batch_id = int(input('Please Enter the Batch Id (from which batch you want the data to start from) in integer: '))
